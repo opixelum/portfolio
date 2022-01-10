c6b5a594275396bf4b3711b151b60aeed1ee1b98
Opixelum <12w0zvkgazd3@opayq.net>
2022-01-10 20:21:09 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="All about my experience">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Anto Benedetti's portofolio | Home</title>
+  </head>
+
+  <body>
+
+  </body>
+</html>
+
