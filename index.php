026deb2507748d95e8de3348c00750269f7d4b19
Opixelum <12w0zvkgazd3@opayq.net>
2022-01-10 21:28:23 +0100

    updated index.php

--- index.php
+++ index.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta name="description" content="All about my experience">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <link rel="stylesheet" href="CSS/style.css">
-    <title>Anto Benedetti's portofolio | Home</title>
   </head>
 
   <body>
-
+    <h1>Welcome to my Portofolio!</h1>    
   </body>
 </html>
 
