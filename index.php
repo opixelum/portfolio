5bc64ac5af067d6076d9ced2959347a4e350a4f6
Opixelum <12w0zvkgazd3@opayq.net>
2022-01-11 07:48:20 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -8,6 +8,41 @@
 
   <body>
     <h1>Welcome to my Portofolio!</h1>    
+
+    <div id="about-me">
+      <h2>About me</h2>
+      <p>
+        Hi there, my name is Anto, I'm 18 years old and I'm a computer science
+        student, attending the ESGI in Paris. Since I was a kid, I've always
+        been passionate about IT, video games, high-tech and more, but my what
+        I'm keen on is programming ! I really like to code a lot, it's like
+        having a super power. My current goal is to work as a developer and learn
+        everyday by being working with experienced programmers.
+      </p>
+    </div>
+    
+    <div id="professional-experience">
+      <h2>My professional experience</h2>
+      <ul>
+        <li>
+          <h3>9th grade internship</h3>
+          <p>FNAC Limoges | February 2018</p>
+        </li>
+      </ul>
+    </div>
+
+    <div id="diplomas">
+      <h2>Diplomas</h2>
+      <ul>
+        <li>
+          <h3>Diplôme National du Brevet des collèges</h3>
+          <p>Mention Très bien | 2017-2018</p>
+        </li>
+        <li>
+          <h3>Diplôme du baccalauréat</h3>
+          <p>Mention Très bien | 2018 - 2021</p>
+        </li>
+      </ul>
+    </div>
   </body>
 </html>
-
