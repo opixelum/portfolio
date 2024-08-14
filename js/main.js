0fc1f4fc36fc31d18d3275b471e1c6f487285ba5
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-08-14 16:00:01 +0200

    Update content

--- js/main.js
+++ js/main.js
@@ -8,16 +8,6 @@
  * @author Anto Benedetti (Opixelum)
  * @bug No known bugs.  */
 
-
-// <###################### F U N C T I O N N A L I T Y #######################>
-
-// <------------------------ C O N T A C T   F O R M ------------------------->
-
-const name = document.getElementById("name")
-const email = document.getElementById("email")
-const message = document.getElementById("message")
-
-
 // <####################### I N T E R A C T I V I T Y ########################>
 
 // <------------------- M A I N   M E N U   B U T T O N S -------------------->
@@ -27,7 +17,7 @@ const aboutMeBtn = document.getElementById("about-me-btn")
 const jobExperiencesBtn = document.getElementById("job-experiences-btn")
 const diplomasBtn = document.getElementById("diplomas-btn")
 const skillsBtn = document.getElementById("skills-btn")
-const personalProjectsBtn = document.getElementById("projects-btn")
+const personalProjectsBtn = document.getElementById("personal-projects-btn")
 
 // Group all divisions in a single array
 const mainMenuBtns = [
@@ -53,14 +43,14 @@ for (let i = 0; i < mainMenuBtns.length; i++) {
 const aboutMeDiv = document.getElementById("about-me-div")
 const jobExperiencesDiv = document.getElementById("job-experiences-div")
 const diplomasDiv = document.getElementById("diplomas-div")
-const projectsDiv = document.getElementById("projects-div")
+const personalProjectsDiv = document.getElementById("personal-projects-div")
 const skillsDiv = document.getElementById("skills-div")
 
 // Group all divisions in a single array
 const divsContent = [
   {
     button: "about-me-btn",
-    id: "about-div",
+    id: "about-me-div",
     content: aboutMeDiv,
   },
   {
@@ -74,9 +64,9 @@ const divsContent = [
     content: diplomasDiv,
   },
   {
-    button: "projects-btn",
-    id: "projects-div",
-    content: projectsDiv,
+    button: "personal-projects-btn",
+    id: "personal-projects-div",
+    content: personalProjectsDiv,
   },
   {
     button: "skills-btn",
@@ -171,36 +161,35 @@ goBackBtn.addEventListener("click", function() {
 // Job list
 const jobExperiences = [
   {
-    title: "Blochain engineer & researcher (apprenticeship)",
+    title: "Ingénieur/Chercheur en Systèmes Distribués (alternance)",
     company: "CEA",
     place: "Paris",
-    date: "September 2022 - September 2024",
+    date: "09/2022 - 09/2024",
     tasks: [
-      "Scientific paper on the computational resource consumption of several smart contract proxy patterns",
-      "Blockchain - based data traceability solutions",
-      "Interactive map to simulate a network of blockchain nodes & weather sensors and calculate its energy consumption",
-      "Experimentation with tools and working methods to establish a common laboratory library"
+      "Papier scientiﬁque sur la consommation de ressources computationnelles de plusieurs proxy patterns de smart contracts.",
+      "Solutions de traçabilité de données à base de blockchains.",
+      "Carte interactive simulant un réseau de nœuds blockchains & de capteurs météorologiques avec analyse de la consommation énergétique."
     ]
   },
   {
-    title: "Web 3 developer (internship)",
+    title: "Développeur Web 3 (stage)",
     company: "Neofacto",
     place: "Paris",
-    date: "May 2022 - August 2022",
+    date: "05/2022 - 08/2022",
     tasks: [
-      "Web 3 application for automating DCA investments in decentralized finance"
+      "Smart contracts pour investissements DCA automatiques.",
+      "Application web décentralisée."
     ]
   },
   {
-    title: "Blockchain developer (freelance)",
+    title: "Développeur Logiciel (freelance)",
     company: "Feature",
     place: "Paris",
-    date: "March 2022 - July 2022",
+    date: "03/2022 - 07/2022",
     tasks: [
-      "Optimization of existing smart contracts",
-      "Testing smart contracts",
-      "Scripting for CI on GitHub",
-      "Writing documentation on GitBook"
+      "Tests de smart contracts.",
+      "Optimisation de smart contracts.",
+      "Scripting pour intégration continue sur GitHub."
     ]
   }
 ]
@@ -218,7 +207,7 @@ for (let i = 0; i < jobExperiences.length; i++) {
   let tasks = ""
   const length = jobExperiences[i].tasks.length
   for (let j = 0; j < length; j++) {
-    tasks += `<li>${jobExperiences[i].tasks[j]}${j == length - 1 ? "." : ";"}</li>`
+    tasks += `<li>${jobExperiences[i].tasks[j]}${j === length - 1 ? "." : ";"}</li>`
   }
   tasksPerJob.push(tasks)
 }
@@ -243,28 +232,19 @@ jobExperiencesUl.innerHTML = jobExperiencesUlContent
 
 const diplomas = [
   {
-    title: "Computer science european master",
-    school: "ESGI Paris",
-    result: "Planned",
-    date: "2024 - 2026"
+    title: 'Master “Expert en ingénierie de l’intelligence artificielle” (à venir)',
+    school: "ESGI",
+    date: "09/2024 - 09/2026"
   },
   {
-    title: "Computer science european bachelor",
-    school: "ESGI Paris",
-    result: "Ongoing",
+    title: 'Bachelor “Chargé de développement de solutions applicatives ou logicielles”',
+    school: "ESGI",
     date: "2021 - 2024"
   },
   {
-    title: "French high school diploma",
-    school: "LGT Robert Weinum Saint Martin",
-    result: "Mention Très bien",
-    date: "2018 - 2021"
-  },
-  {
-    title: "French junior high school certificate",
-    school: "Collège Jean Moulin Ambazac",
-    result: "Mention Très bien",
-    date: "2017-2018"
+    title: "Bac Général",
+    school: "LGT Robert Weinum",
+    date: "09/2018 - 07/2021"
   }
 ]
 
@@ -279,8 +259,7 @@ for (let i = 0; i < diplomas.length; i++) {
   diplomasUlContent += `
     <li>
       <h3>${diplomas[i].title}</h3>
-      <p>${diplomas[i].school}<br>${diplomas[i].result}<br>
-      ${diplomas[i].date}</p>
+      <p>${diplomas[i].school}<br>${diplomas[i].date}</p>
     </li>`
 }
 
@@ -290,70 +269,74 @@ diplomasUl.innerHTML = diplomasUlContent
 
 // <---------------------------- P R O J E C T S ----------------------------->
 
-const projects = [
+const personalProjects = [
   {
-    title: "Convopy",
-    brief: "Convolutional neural network for image classification.",
-    date: "2024",
-    url: "https://github.com/opixelum/convopy",
+    title: "Atamai",
+    brief: "Librairie de machine learning en Rust avec interface Python.",
+    date: "07/2024 - Présent",
+    url: "https://github.com/opixelum/atamai",
   },
   {
     title: "Opixelib",
-    brief: "Personal, independent, multi-language library with sorting, math and artificial intelligence modules.",
-    date: "2024",
+    brief: "Librairie multi-langages comprenant des modules de tri et de maths.",
+    date: "10/2023 - Présent",
     url: "https://github.com/opixelum/opixelib",
   },
   {
-    title: "Java Simple Neural Network",
-    brief: "First implementation of a neural network in Java for easy tasks.",
-    date: "2023",
-    url: "https://github.com/opixelum/java-simple-neural-network"
+    title: "ALIA",
+    brief: "Application de classiﬁcation d’arachnides avec notre propre librairie de machine learning en Rust et interface Python.",
+    date: "03/2024 - 07/2024",
+    url: "https://github.com/alia-team/ai",
   },
   {
-    title: "ChatGPT CLI",
-    brief: "Command-line interface for ChatGPT.",
-    date: "2023",
-    url: "https://github.com/opixelum/chatgpt-cli",
+    title: "Kaggle",
+    brief: "Divers projets de machine learning (régression, classification, ...).",
+    date: "01/2024 - Présent",
+    url: "https://kaggle.com/antobenedetti",
   },
-  {
-    title: "Vault",
-    brief: "Password manager developed in C.",
-    date: "2023",
-    url: "https://github.com/opixelum/vault",
-  }
 ]
 
 // Taking container from the DOM
-const projectsUl = document.getElementById("projects-ul")
+const personalProjectsUl = document.getElementById("personal-projects-ul")
 
 // Reset projects container content
-let projectsUlContent = ""
+let personalProjectsUlContent = ""
 
 // Add projects to the container
-for (let i = 0; i < projects.length; i++) {
-  projectsUlContent += `
+for (let i = 0; i < personalProjects.length; i++) {
+  personalProjectsUlContent += `
     <li>
-      <h3>${projects[i].title}</h3>
-      <p>${projects[i].brief}<br><br>${projects[i].date}<br><br>
-      <a href="${projects[i].url}" target="_blank">Click here to learn more</a>
+      <h3>${personalProjects[i].title}</h3>
+      <p>${personalProjects[i].brief}<br><br>${personalProjects[i].date}<br><br>
+      <a href="${personalProjects[i].url}" target="_blank">Click here to learn more</a>
       </p>
     </li>`
 }
 
 // Modify HTML with new projects list
-projectsUl.innerHTML = projectsUlContent
+personalProjectsUl.innerHTML = personalProjectsUlContent
 
 
 // <------------------------------ S K I L L S ------------------------------->
 
 const skills = [
+  "Python",
   "Rust",
   "C",
   "Solidity",
-  "Python",
+  "JavaScript",
   "TypeScript",
   "TensorFlow",
-  "Deep Learning"
+  "PyTorch",
+  "SQL",
+  "Cassandra",
+  "Hadoop",
+  "PySpark",
+  "Neo4J",
+  "PowerBI",
+  "Excel",
+  "Linux",
+  "Windows",
 ]
 
 // Taking container from the DOM
